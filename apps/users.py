ef5f2311fdc369e6f3d956149c759b2048f1180d
Ekaterina Voloshina <42929264+EkaterinaVoloshina@users.noreply.github.com>
2022-03-27 01:25:02 +0300

    Update users.py

--- apps/users.py
+++ apps/users.py
@@ -9,11 +9,10 @@ authenticator = stauth.Authenticate(names,usernames,hashed_passwords,
     'some_cookie_name','some_signature_key',cookie_expiry_days=30)
 
 def app():
-    st.title('Users page')
     name, authentication_status, username = authenticator.login('Login', 'main')
     if authentication_status:
         st.write('Welcome, *%s*' % (name))
-        st.title('Here you can delete, change data or add new poets.')
+        st.header('Here you can delete, change data or add new poets.')
     elif authentication_status == False:
         st.error('Username/password is incorrect')
     elif authentication_status == None:
