e9c6bd8cada12cf9c84771d83c5d51e852838011
Ekaterina Voloshina <42929264+EkaterinaVoloshina@users.noreply.github.com>
2022-03-27 23:43:58 +0300

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -11,20 +11,57 @@ def app():
     st.text('увлеченным литературой учащимся наш ресурс поможет открыть много нового.')
     
     st.header('НАША КОМАНДА')
-    
-    col1, col2, col3 = st.columns([5, 5, 5])
-    with col1:
-        st.text('Анна Аксенова')
-    with col2:
-        st.image('img/me.png')
-        st.text('Екатерина Волошина')
-    with col3:
-        st.image('img/polina.png')
-        st.text('Полина Кудрявцева')
-    col1, col2 = st.columns([7, 7])
-    with col1:
-        st.image('img/katya.png')
-        st.text('Екатерина Такташева')
-    with col2:
-        st.text('Екатерина Тарасова')
+    st.markdown("""
+    <div class="row align-items-start" style="margin-bottom:2%">
+                <div class="col">
+                    <div class="d-grid gap-0">
+                        <div class="circular-img mx-auto d-block">
+                            <img src="../static/images/anya.jpg" class="img-fluid circular-portrait" style="margin-top:-2vh">
+                        </div>
+                        <div class="p-2 fs-4 fw-bold text-center lh-sm">Аксенова Анна</div>
+                        <div class="p-2 fs-6 fw-lighter text-center lh-sm">Разработка автоматической разметки</div>
+                    </div>
+                </div>
+                <!-- Person 2-->
+                <div class="col">
+                   <div class="d-grid gap-0">
+                       <div class="circular-img mx-auto d-block">
+                           <img src="../static/images/katya_v.jpg" class="img-fluid circular-portrait" style="margin-top:-5.5vh">
+                       </div>
+                       <div class="p-2 fs-4 fw-bold text-center lh-sm">Волошина Екатерина</div>
+                       <div class="p-2 fs-6 fw-lighter text-center lh-sm">Разработка автоматической разметки</div>
+                    </div>
+                </div>
+                <!-- Person 3 -->
+                <div class="col">
+                    <div class="d-grid gap-0">
+                        <div class="circular-img mx-auto d-block">
+                            <img src="../static/images/polina.jpg" class="img-fluid circular-portrait" style="margin-top:-2vh">
+                        </div>
+                        <div class="p-2 fs-4 fw-bold text-center lh-sm">Кудрявцева Полина</div>
+                        <div class="p-2 fs-6 fw-lighter text-center lh-sm">Разработка автоматической разметки</div>
+                    </div>
+                </div>
+            </div>
+            <!-- Person 4 -->
+            <div class="row justify-content-evenly">
+                <div class="col-4">
+                    <div class="d-grid gap-0">
+                        <div class="circular-img mx-auto d-block">
+                            <img src="../static/images/katya_tak.jpeg" class="img-fluid circular-portrait" style="margin-top:-1vh">
+                        </div>
+                        <div class="p-2 fs-4 fw-bold text-center lh-sm">Такташева Екатерина</div>
+                        <div class="p-2 fs-6 fw-lighter text-center lh-sm">Создание корпуса и сайта, техническая поддержка</div>
+                    </div>
+                </div>
+                <!-- Person 5 -->
+                <div class="col-4">
+                    <div class="d-grid gap-0">
+                        <div class="circular-img mx-auto d-block">
+                            <img src="../static/images/katya_tar.jpg" class="circular-landscape" style="margin-left:-1vw">
+                        </div>
+                        <div class="p-2 fs-4 fw-bold text-center">Тарасова Екатерина</div>
+                        <div class="p-2 fs-6 fw-lighter text-center">Филологический комментарий</div>
+                    </div>
+                </div>""")
     
