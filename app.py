dbd2547b49cd69a05609bf7dc2b54b6050db6431
Ekaterina Voloshina <42929264+EkaterinaVoloshina@users.noreply.github.com>
2022-03-27 21:40:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import pandas as pd
 from multiapp import MultiApp
-
+import pymongo 
 from apps import home, search, poets, users, graphs # import your app modules here
 
+def init_connection():
+    return pymongo.MongoClient(**st.secrets["mongo"])
+
+client = init_connection()
 app = MultiApp()
 
 app.add_app("Home", home.app)
